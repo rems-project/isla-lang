413f1d1c7fa725d757662d044dcebbf9d2e57d2e
Brian Campbell <Brian.Campbell@ed.ac.uk>
2022-03-03 11:04:12 +0000

    Fix for compatibility with recent pprint

--- main.ml
+++ main.ml
@@ -55,16 +55,16 @@ type ast = AST.lrng AST.trcs
 type tree_ast = AST.lrng AST.tree_trc
 
 let pp_pretty : out_channel -> ast -> unit = fun oc ast ->
-  PPrintEngine.ToChannel.compact oc (PP.pp_trcs ast)
+  PPrint.ToChannel.compact oc (PP.pp_trcs ast)
 
 let pp_pretty_tree : out_channel -> tree_ast -> unit = fun oc ast ->
-  PPrintEngine.ToChannel.compact oc (PP.pp_tree_trc ast)
+  PPrint.ToChannel.compact oc (PP.pp_tree_trc ast)
 
 let pp_raw : out_channel -> ast -> unit = fun oc ast ->
-  PPrintEngine.ToChannel.compact oc (PP.pp_raw_trcs ast)
+  PPrint.ToChannel.compact oc (PP.pp_raw_trcs ast)
 
 let pp_raw_tree : out_channel -> tree_ast -> unit = fun oc ast ->
-  PPrintEngine.ToChannel.compact oc (PP.pp_raw_tree_trc ast)
+  PPrint.ToChannel.compact oc (PP.pp_raw_tree_trc ast)
 
 let parse : Lexing.lexbuf -> ast = fun lexbuf ->
   Parser.trcs_start Lexer.token lexbuf
